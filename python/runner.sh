01a6650a4487e634905febc281363762e212366a
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2017-01-17 21:07:24 +0100

    housekeeping

--- python/runner.sh
+++ python/runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ./run_ckm.py ../temp/examples/iris.data ../temp/examples/iris.constraints 3 /dev/null
+python ./run_ckm.py ../data/iris.data ../data/iris.constraints 3 /dev/null
